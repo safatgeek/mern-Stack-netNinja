5fad15b617c9c842a70bdb18576412621a0cad6e
safatgeek <safatd310e@gmail.com>
2023-08-18 23:29:56 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const express = require('express')
+const workoutRoutes = require('./routes/workouts')
+
+// express app
+const app = express()
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//routes
+app.use('/api/workouts', workoutRoutes)
+
+// routes
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the app !!!'})
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT)
+})
